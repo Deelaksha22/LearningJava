ec39a8ba0799a5ed9a540c7ac3cc0ce6489bb6cf
Deelaksha NJ <deelaksha22@gmail.com>
2026-07-21 09:02:58 +0530

    Java datatype

---
+++ DataType.java
@@ -0,0 +1,14 @@
+public class DataType {
+    public  static void main(String[]  args){
+    System.out.println("Printing data types  in java");
+    int a =10;
+    int b =20;
+    int c = a+b;
+   System.out.println(c);
+   System.out.println(c++);
+   System.out.println(c);
+   System.out.println(++c);
+   char ch ='z';
+   System.out.println(++ch);
+    }
+}
--- Input.java
+++ Input.java
@@ -1,10 +1,12 @@
-// import java.util.Scanner;
-// public class Input {
-//     public static void main(String[] args) {
-//         Scanner scanner = new Scanner(System.in);
-//         System.out.print("Enter your name: ");
-//         String name = scanner.nextLine();
-//         System.out.println("Hello, " + name + "!");
-//     }
-    
-// }
+import java.util.Scanner;
+
+public class Input {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
+
+}
